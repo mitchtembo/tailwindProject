8621c274f792fcc74715a4a46384ee0b51c40a20
mitchtembo <git@stackblitz.com>
2024-10-20 21:56:59 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css'; // Adjust the path if needed
+
+function App() {
+  return <div className="bg-blue-500 text-red-500 p-4">Hello, Tailwind!</div>;
+}
+
+export default App;
