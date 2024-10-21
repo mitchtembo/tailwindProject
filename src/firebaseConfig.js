4f99da07209a6527feb03aa7d178ac95215d5103
mitchtembo <mitchtembo@gmail.com>
2024-10-21 10:15:58 +0200

    API securely stored

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,15 +1,11 @@
 // src/firebaseConfig.js
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-
-// firebaseConfig.js
 export const firebaseConfig = {
-  apiKey: "AIzaSyAlSMlC-26C5UCpuCiirCdOl4OYp-ZJWPE",
-  authDomain: "dronepilotmanager.firebaseapp.com",
-  projectId: "dronepilotmanager",
-  storageBucket: "dronepilotmanager.appspot.com",
-  messagingSenderId: "660510890627",
-  appId: "1:660510890627:web:b22417841a62a93b3859b6",
-  measurementId: "G-YXMJS98TJQ",
-};
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  };
+  
